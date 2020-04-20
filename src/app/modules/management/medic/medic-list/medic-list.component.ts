c279d28f064b28c71f622547fe7409abed721ba0
Jose Luis Rego <regorodrijl@gmail.com>
2020-04-20 20:22:26 +0200

    #147427 Incluidos unos pollifils y modal de bootstrap. Primera modal, sin fomulario.

--- src/app/modules/management/medic/medic-list/medic-list.component.ts
+++ src/app/modules/management/medic/medic-list/medic-list.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { Component, OnInit, ViewChild, TemplateRef } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 import { TranslateService } from '@ngx-translate/core';
 import { MedicService } from '../../services/medic/medic.service';
 import { ToastrService } from 'ngx-toastr';
+import { ModalComponent } from 'src/app/core/components/modal/modal.component';
+import { INFO_MODAL_CONSTANT } from './INFO_MODAL_CONSTANT';
 
 @Component({
   selector: 'app-medic-list',
@@ -14,17 +17,52 @@ export class MedicListComponent implements OnInit {
     searcherForm: new FormControl()
   });
 
-  elementoSeleccionado = null;
+  @ViewChild('infoModal') public infoModal;
+  @ViewChild('editModal') public editModal;
+  @ViewChild("editModal", { static: false }) myModalInfo: TemplateRef<any>;
+
+
+  modalForm: FormGroup;
+
+  public elementoSeleccionado: any = null;
+  public show = true;
+  public isDetailModal = false;
+  public isEditModal = false;
+  public isNewModal = false;
 
   constructor(
     public translate: TranslateService,
     public medicService: MedicService,
-    private _toastr: ToastrService
+    private _toastr: ToastrService,
+    private _formBuilder: FormBuilder,
+    private modalService: NgbModal
   ) { }
-
+  public medic = {
+    name: "Prueba",
+    surname: "",
+    phone: "",
+    dni: "",
+    collegeNumber: "",
+    active: "",
+    username: "",
+    password: "",
+    email: "",
+  }
   ngOnInit() {
+    this.modalForm = this._formBuilder.group({
+      name: ['', Validators.required],
+      surname: ['', Validators.required],
+      // phone: ['', Validators.required],
+      // dni: ['', Validators.required],
+      // collegeNumber: ['', Validators.required],
+      // active: [Boolean, Validators.required],
+      // username: ['', Validators.required],
+      // password: ['', Validators.required],
+      // email: ['', Validators.required]
+    });
+
     this.medicService.getAll().subscribe(result => {
-      debugger
+
     },
       error => {
         this._toastr.error(error.status + " " + error.statusText);
@@ -36,5 +74,28 @@ export class MedicListComponent implements OnInit {
   }
   showModal(data: any) {
 
+    this.isNewModal = data.isNew;
+    this.isEditModal = data.isEdit;
+    this.isDetailModal = data.isDetail;
+
+    const modalRef = this.modalService.open(ModalComponent);
+
+    modalRef.componentInstance.dataModal = this.medic;
+    modalRef.componentInstance.title = "prueba";
+    modalRef.componentInstance.modalData = INFO_MODAL_CONSTANT.STOP_EDITING_MODAL_DATA;
+    modalRef.componentInstance.emitInfo
+      .subscribe({ next: this._confirmToStop });
+    modalRef.result.then((result) => {
+      console.log(result);
+    }).catch((error) => {
+      console.log(error);
+    });
+
+  }
+  private _confirmToStop = (response: { opStatus: string }) => {
+    if (response.opStatus.toLowerCase() === 'confirmed') {
+      // do operation
+      console.log('Perform operation here!');
+    }
   }
 }
