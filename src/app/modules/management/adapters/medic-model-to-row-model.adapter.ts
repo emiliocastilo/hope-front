1786cfe0d61fa2ddaca10bed421bdaa1646f3762
Jose Luis Rego <regorodrijl@gmail.com>
2020-04-20 20:49:43 +0200

    #147425 Insertando tabla. con datos de prueba.

---
+++ src/app/modules/management/adapters/medic-model-to-row-model.adapter.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { RowDataModel } from 'src/app/core/models/table/row-data.model';
+import { MedicModel } from '../models/medic.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MedicModelToRowModelAdapter {
+
+  constructor() { }
+
+  public adaptModelToRow(medic: MedicModel): RowDataModel {
+    let row = new RowDataModel();
+    row.pushColumn(medic.name);
+    row.pushColumn(medic.surname);
+    row.pushColumn(medic.dni);
+    row.pushColumn(medic.phone);
+    row.pushColumn(medic.collegeNumber);
+
+    return row;
+  }
+}
---
+++ src/app/modules/management/models/medic.model.ts
@@ -0,0 +1,9 @@
+export class MedicModel {
+    constructor(
+        public name: string,
+        public surname: string,
+        public dni: string,
+        public phone: string,
+        public collegeNumber: string,
+    ) { }
+} 
