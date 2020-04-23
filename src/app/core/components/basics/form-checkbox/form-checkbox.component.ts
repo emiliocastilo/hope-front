183134c4671926ccd44bb1d0973dc572a29c587f
Jose Luis Rego <regorodrijl@gmail.com>
2020-04-23 14:46:25 +0200

    #148547 Primer Checkbox de formulario

---
+++ src/app/core/components/basics/form-checkbox/form-checkbox.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+import { FieldConfig } from 'src/app/core/interfaces/dynamic-forms/field-config.interface';
+
+@Component({
+  selector: 'app-form-checkbox',
+  templateUrl: './form-checkbox.component.html',
+  styleUrls: ['./form-checkbox.component.sass']
+})
+export class FormCheckboxComponent {
+  config: FieldConfig;
+  group: FormGroup;
+}
