9db781eb4aa68642408a2c7dedbf6e02bedc74ed
Jose Luis Rego <regorodrijl@gmail.com>
2020-04-23 14:47:06 +0200

    #148547 Primer Select de formulario (dropdown o desplegable.)

---
+++ src/app/core/components/basics/form-select/form-select.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+import { FieldConfig } from 'src/app/core/interfaces/dynamic-forms/field-config.interface';
+
+@Component({
+  selector: 'app-form-select',
+  templateUrl: './form-select.component.html',
+  styleUrls: ['./form-select.component.sass']
+})
+export class FormSelectComponent {
+  config: FieldConfig;
+  group: FormGroup;
+}
