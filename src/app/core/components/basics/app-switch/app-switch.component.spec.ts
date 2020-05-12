8f8cdf10d8fb3418f03ccd00e3fc758947845aba
Jose Luis Rego <regorodrijl@gmail.com>
2020-05-12 17:30:40 +0200

    Arreglando erroes de nombres en switch

--- src/app/core/components/basics/app-switch/app-switch.component.spec.ts
+++ src/app/core/components/basics/app-switch/app-switch.component.spec.ts
@@ -1,19 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AppSwitchComponent } from './app-switch.component';
+import { SwitchComponent } from './app-switch.component';
 
-describe('AppSwitchComponent', () => {
-  let component: AppSwitchComponent;
-  let fixture: ComponentFixture<AppSwitchComponent>;
+describe('SwitchComponent', () => {
+  let component: SwitchComponent;
+  let fixture: ComponentFixture<SwitchComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [AppSwitchComponent],
+      declarations: [SwitchComponent],
     }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AppSwitchComponent);
+    fixture = TestBed.createComponent(SwitchComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
