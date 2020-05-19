91ec0550981d0270c031aaf423d20e1c29babf19
elenapereiravazquez <elena.pereiravazquez@plexus.es>
2020-05-19 18:00:44 +0200

    pie chart component created

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,6 +5,7 @@ import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { HttpClient } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 import { ButtonComponent } from './components/basics/button/button.component';
 import { InputComponent } from './components/basics/input/input.component';
@@ -43,6 +44,7 @@ import { InputFileComponent } from './components/basics/input-file/input-file.co
 import { RouterModule } from '@angular/router';
 import { NgbdSortableHeader } from './directives/sortable.directive';
 import { FormsComponent } from './components/forms/forms.component';
+import { PieChartComponent } from './components/charts/pie-chart/pie-chart.component';
 
 export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -85,12 +87,14 @@ export function createTranslateLoader(http: HttpClient) {
     InputFileComponent,
     NgbdSortableHeader,
     FormsComponent,
+    PieChartComponent,
   ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
     FormsModule,
     NgbModule,
+    NgxChartsModule,
     RouterModule,
     TranslateModule.forChild({
       loader: {
@@ -125,6 +129,7 @@ export function createTranslateLoader(http: HttpClient) {
     EditorModalComponent,
     FormSwitchComponent,
     SwitchComponent,
+    PieChartComponent,
     PaginationComponent,
     HomeDashboardModuleComponent,
   ],
