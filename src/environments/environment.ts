ff256689f11bdff6e72ec523b413485031122623
erickGonzalez <erickfat19@gmail.com>
2020-05-29 09:03:57 +0200

    f/#000: se cambia el puerto 8080 por 8082 del back a peticion de jordi.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // URL_API: 'http://192.168.110.45:8080/v1',
-  URL_API: ' http://localhost:8080/v1',
+  // URL_API: 'http://192.168.110.45:8082/v1',
+  URL_API: ' http://localhost:8082/v1',
 };
 
 /*
