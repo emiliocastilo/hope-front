8bca7e045ad12c43b2da01d81bec1b547ce9a41c
elenapereiravazquez <elena.pereiravazquez@plexus.es>
2020-06-01 14:30:37 +0200

    calculated front component pending testing

--- src/app/core/components/basics/form-calculated/form-calculated.component.ts
+++ src/app/core/components/basics/form-calculated/form-calculated.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
 import { FieldConfig } from 'src/app/core/interfaces/dynamic-forms/field-config.interface';
--- src/app/core/utils/FormUtils.ts
+++ src/app/core/utils/FormUtils.ts
@@ -1,6 +1,7 @@
 import { FieldConfig } from '../interfaces/dynamic-forms/field-config.interface';
 import { FieldConfigModel } from '../models/forms/field-config.model';
 import StringUtils from './StringUtils';
+import moment from 'moment';
 
 export default class FormUtils {
   static createFieldConfig(form, filled?): FieldConfig[] {
@@ -61,5 +62,9 @@ export default class FormUtils {
       form.push(entry);
     });
     return form;
+  }
+
+  static ageBybirthdate(date) {
+    return moment().diff(date, 'years');
   }
 }
