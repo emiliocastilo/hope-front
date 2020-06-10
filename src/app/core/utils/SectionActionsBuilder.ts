455f93f7d65a9d7ca28f0eae9c29a127dbbf8f6a
elenapereiravazquez <elena.pereiravazquez@plexus.es>
2020-06-10 11:09:48 +0200

    rebase breadcrumbs release

--- src/app/core/utils/SectionActionsBuilder.ts
+++ src/app/core/utils/SectionActionsBuilder.ts
@@ -2,39 +2,58 @@ import { SideBarItemModel } from 'src/app/core/models/side-bar/side-bar-item.mod
 import { SectionsService } from 'src/app/modules/management/services/sections/sections.service';
 
 export default class SectionActionBuilder {
-  constructor(
-    public menu: SideBarItemModel[],
-    private sectionsService?: SectionsService
-  ) {
-    this.menu = menu;
+  constructor(private sectionsService?: SectionsService) {
     this.sectionsService = sectionsService;
   }
 
-  private selected: SideBarItemModel;
-
-  public getSelectedByUrl(url: string): SideBarItemModel {
-    this.selectedByUrl(url, this.menu);
-
-    return this.selected;
-  }
-
-  private selectedByUrl(
-    url: string,
-    menu: SideBarItemModel[]
-  ): SideBarItemModel {
-    let selectedArray = menu.filter((item: SideBarItemModel) =>
-      item.url.includes(url)
-    );
-
-    selectedArray = selectedArray.length
-      ? selectedArray
-      : menu.map((item: SideBarItemModel) =>
-          this.selectedByUrl(url, item.children)
+  public findSection(type: string, array: Array<any>, key: any) {
+    let result: any;
+    switch (type) {
+      case 'id':
+        array.some(
+          (o) =>
+            (result =
+              o.id === key ? o : this.findSection('id', o.children || [], key))
         );
-
-    this.selected = url === '/' ? null : selectedArray[0];
-    return this.selected;
+        return result;
+      case 'url':
+        array.some(
+          (o) =>
+            (result = o.url.includes(key)
+              ? o
+              : this.findSection('url', o.children || [], key))
+        );
+        return result;
+      default:
+        break;
+    }
   }
+
+  // private selected: SideBarItemModel;
+
+  // public getSelectedByUrl(url: string): SideBarItemModel {
+  //   this.selectedByUrl(url, this.menu);
+
+  //   return this.selected;
+  // }
+
+  // private selectedByUrl(
+  //   url: string,
+  //   menu: SideBarItemModel[]
+  // ): SideBarItemModel {
+  //   let selectedArray = menu.filter((item: SideBarItemModel) =>
+  //     item.url.includes(url)
+  //   );
+
+  //   selectedArray = selectedArray.length
+  //     ? selectedArray
+  //     : menu.map((item: SideBarItemModel) =>
+  //         this.selectedByUrl(url, item.children)
+  //       );
+
+  //   this.selected = url === '/' ? null : selectedArray[0];
+  //   return this.selected;
+  // }
 
   public getCrumbs(section: SideBarItemModel): SideBarItemModel[] {
     return this.pushCrumbs(section, []).reverse();
