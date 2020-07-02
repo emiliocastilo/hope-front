d5dca1ca3d5351f1d0f727c76c0d7ed4c7b04655
erickGonzalez <erickfat19@gmail.com>
2020-07-02 15:58:01 +0200

    f/#154686: crear componente de perfil de usuario.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -59,6 +59,7 @@ import { PatientHeaderComponent } from './components/patient-header/patient-head
 import { FormHistoricComponent } from './components/basics/form-historic/form-historic.component';
 import { FromSectionComponent } from './components/basics/from-section/from-section.component';
 import { ManyChartModalComponent } from './components/modals/many-chart-modal/many-chart-modal.component';
+import { MyAccountComponent } from './components/my-account/my-account.component';
 
 export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -116,6 +117,7 @@ export function createTranslateLoader(http: HttpClient) {
     LineChartComponent,
     FormHistoricComponent,
     ManyChartModalComponent,
+    MyAccountComponent,
   ],
   imports: [
     CommonModule,
