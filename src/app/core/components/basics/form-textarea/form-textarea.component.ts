d223950efc6662bc839cbc9f743c29adc04e26ce
ero.rodriguezgrande <ero.rodriguezgrande@plexus.es>
2020-07-08 11:29:58 +0200

    create text area obligatorios

--- src/app/core/components/basics/form-textarea/form-textarea.component.ts
+++ src/app/core/components/basics/form-textarea/form-textarea.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FieldConfig } from 'src/app/core/interfaces/dynamic-forms/field-config.interface';
-import { FormGroup } from '@angular/forms';
+import { AbstractControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form-textarea',
   templateUrl: './form-textarea.component.html',
   styleUrls: ['./form-textarea.component.scss'],
 })
-export class FormTextareaComponent {
+export class FormTextareaComponent implements OnInit {
   config: FieldConfig;
   group: FormGroup;
   rows: number = this.config?.rows | 1;
+  required: boolean = false;
+
+  ngOnInit() {
+    this.hasRequiredField(this.group.controls[this.config.name]);
+  }
+
+  hasRequiredField(abstractControl: AbstractControl) {
+    if (abstractControl.validator) {
+      const validator = abstractControl.validator({} as AbstractControl);
+      if (validator && validator.required) {
+        this.required = true;
+      }
+    }
+  }
 }
