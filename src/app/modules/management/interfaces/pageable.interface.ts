4ea4801e33933a94ee1e6da3fdf83b69c2484543
sineiroplexus <lorena.sineirogarcia@plexus.es>
2020-11-17 17:33:46 +0100

    fixing breadcrumps

--- src/app/modules/management/interfaces/pageable.interface.ts
+++ src/app/modules/management/interfaces/pageable.interface.ts
@@ -1,10 +1,10 @@
 import { Sort } from './sort.interface';
 
 export interface Pageable {
-    sort: Sort,
-    offset: number,
-    pageNumber: number,
-    pageSize: number,
-    paged: boolean,
-    unpaged: boolean
+  sort: Sort;
+  offset: number;
+  pageNumber: number;
+  pageSize: number;
+  paged: boolean;
+  unpaged: boolean;
 }
