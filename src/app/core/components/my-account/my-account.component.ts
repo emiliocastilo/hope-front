e04abe6bf19a872ca93db4ea557f3f479b967f3d
Lorena Sineiro García <lorena.sineirogarcia@plexus.es>
2020-11-24 13:05:22 +0100

    loged user check on my-account-component

--- src/app/core/components/my-account/my-account.component.ts
+++ src/app/core/components/my-account/my-account.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { RolModel } from 'src/app/modules/management/models/rol.model';
 import { UserModel } from 'src/app/modules/management/models/user/user.model';
@@ -21,6 +22,7 @@ export class MyAccountComponent implements OnInit {
   constructor(
     private _roleListener: CurrentRoleListenerService,
     private _formBuilder: FormBuilder,
+    private _router: Router,
     private _modalService: NgbModal
   ) {}
 
@@ -29,10 +31,11 @@ export class MyAccountComponent implements OnInit {
   }
 
   private getCurrentuser() {
-    this.currentUser = JSON.parse(localStorage.getItem('user') || '{}');
-    // this.currentRole = JSON.parse(localStorage.getItem('role') || '{}');
-    this.currentRole = this.currentUser.rolSelected;
-    this.userRoles = this.currentUser.roles;
+    this.currentUser = JSON.parse(localStorage.getItem('user') || undefined);
+
+    if (this.currentUser && this.currentUser.rolSelected)
+      this.currentRole = this.currentUser.rolSelected;
+    else this._router.navigate(['/login']);
 
     const allRoles: Array<RolModel> = JSON.parse(localStorage.getItem('roles'));
     if (allRoles && allRoles.length > 0)
