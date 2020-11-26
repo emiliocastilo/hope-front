b4d561b5eaacde3e36a05657a2255de45ce8352e
Lorena Sineiro García <lorena.sineirogarcia@plexus.es>
2020-11-26 12:49:03 +0100

    wip menu fix

--- src/app/core/components/side-bar/side-bar-menu/side-bar-menu.component.ts
+++ src/app/core/components/side-bar/side-bar-menu/side-bar-menu.component.ts
@@ -6,150 +6,177 @@ import { Subscription } from 'rxjs';
 import SectionActionBuilder from 'src/app/core/utils/SectionActionsBuilder';
 
 @Component({
-  selector: 'side-bar-menu',
-  templateUrl: './side-bar-menu.component.html',
-  styleUrls: ['./side-bar-menu.component.scss'],
+    selector: 'side-bar-menu',
+    templateUrl: './side-bar-menu.component.html',
+    styleUrls: ['./side-bar-menu.component.scss'],
 })
 export class SideBarMenuComponent implements OnInit, OnDestroy {
-  private currentSectionSubscription: Subscription;
-  private sectionSecuence: Array<SideBarItemModel>;
+    private currentSectionSubscription: Subscription;
+    // private sectionSequence: Array<SideBarItemModel>;
+    private homeUrl = '/hopes';
 
-  public LEVEL_ONE = 1;
-  public icons: any;
-  public ICONS = {
-    Administración: 'settings',
-    'Cuadro de Mando': 'bar-chart-2',
-    Calendario: 'calendar',
-    Alertas: 'bell',
-    Paciente: 'users',
-    Soporte: 'mail',
-  };
+    public LEVEL_ONE = 1;
+    public icons: any;
+    public ICONS = {
+        Administración: 'settings',
+        'Cuadro de Mando': 'bar-chart-2',
+        Calendario: 'calendar',
+        Alertas: 'bell',
+        Paciente: 'users',
+        Soporte: 'mail',
+    };
 
-  currentSection: SideBarItemModel;
+    currentSection: SideBarItemModel;
 
-  @Input() parentSection: SideBarItemModel;
-  @Input() menu: Array<SideBarItemModel>;
-  @Input() level: number;
-  @Input() collapsed: boolean;
+    @Input() parentSection: SideBarItemModel;
+    @Input() menu: Array<SideBarItemModel>;
+    @Input() level: number;
+    @Input() collapsed: boolean;
 
-  constructor(private _sidebar: SideBarService, private _router: Router) {}
+    constructor(private _sidebar: SideBarService, private _router: Router) { }
 
-  ngOnInit(): void {
-    this.currentSection = JSON.parse(localStorage.getItem('section'));
-    this.sectionSecuence = SectionActionBuilder.getCrumbs(this.currentSection);
-    this.sectionSecuence.shift();
+    ngOnInit (): void {
+        this.currentSection = JSON.parse(localStorage.getItem('section') || '{}');
+        // console.log('INITIAL SECTION :: ', this.currentSection.title, this.sectionSecuence);
+        // console.log('Nivel', this.level, this.sectionSecuence[this.level - 1]);
 
-    // console.log('INITIAL SECTION :: ', this.currentSection.title, this.sectionSecuence);
-    // console.log('Nivel', this.level, this.sectionSecuence[this.level - 1]);
+        this.currentSectionSubscription = this._sidebar
+            .getCurrentSection()
+            .subscribe((section: SideBarItemModel) => {
+                this.currentSection = section;
+                // this.sectionSequence = SectionActionBuilder.getCrumbs(this.currentSection);
+                // this.sectionSequence.shift();
+                this.collapseAll(this.menu);
+                this.updateCollapseState(this.menu);
+                // this.checkCollapsedSection(section);
+            });
 
-    this.sectionSecuence.forEach((element) => {});
-
-    this.currentSectionSubscription = this._sidebar
-      .getCurrentSection()
-      .subscribe((section: SideBarItemModel) => {
-        this.currentSection = section;
-        this.updateCollapseState(this.menu);
-        // this.checkCollapsedSection(section);
-      });
-
-    setTimeout(() => {
-      if (this.menu) {
-        // console.log('menu', this.menu);
-        // console.log('level', this.level);
-        this.assingParents();
-        // console.log(this.menu);
-        // this.checkCollapsedSection(this.currentSection);
-        this.updateCollapseState(this.menu);
-      }
-    }, 500);
-    if (!this.level) {
-      this.level = this.LEVEL_ONE;
+        setTimeout(() => {
+            if (this.menu) {
+                this.assingParents();
+                // this.sectionSequence = SectionActionBuilder.getCrumbs(this.currentSection);
+                if (this.currentSection) {
+                    // this.sectionSequence.shift();
+                    this.updateCollapseState(this.menu);
+                } else this.menu.forEach(item => item.collapsed = true);
+                // console.log('menu', this.menu);
+                // console.log('level', this.level);
+                // console.log(this.menu);
+                // this.checkCollapsedSection(this.currentSection);
+            }
+        }, 500);
+        if (!this.level) {
+            this.level = this.LEVEL_ONE;
+        }
     }
-  }
 
-  goUrl(section: SideBarItemModel) {
-    this._sidebar.currentSection.next(section);
-    event.preventDefault();
-    // if (section.children.length) {
-    //     this.toggleColapseMenu(section);
-    // }
-    const url = section.url.split('hopes')[1];
-    if (url) {
-      this._router.navigate([url]);
+    collapseAll (menu?: Array<SideBarItemModel>) {
+        if (menu && menu.length > 0) {
+            menu.forEach(element => {
+                element.collapsed = true;
+                if (element.children && element.children.length > 0) this.collapseAll(element.children);
+            });
+        }
     }
-    // this.currentSection = section;
-    // this._sidebar.currentSection.next(section);
-    this.updateCollapseState(this.menu);
-  }
 
-  public toggleColapseMenu(menuItem: SideBarItemModel): void {
-    menuItem.collapsed = !menuItem.collapsed;
-  }
+    goUrl (section: SideBarItemModel) {
+        const url = section.url === this.homeUrl ? this.homeUrl : section.url.split('hopes')[1];
 
-  checkCollapsedSection(section: SideBarItemModel) {
-    if (section && section.fatherSection !== null) {
-      this.checkCollapsedSection(section.fatherSection);
-      this.activateCollapse(this.menu, section.id);
+        // if (url) this._router.navigate([url]).then(
+        //     () => {
+        //         debugger
+        //         console.log('THEN DEL NAVIGATE');
+        //         this._sidebar.currentSection.next(section);
+        //     }
+        // );
+
+        if (url) this._router.navigate([url]);
+        setTimeout(() => this._sidebar.currentSection.next(section), 150);
+
+        // this.collapseAll();
+        // this.updateCollapseState(this.menu);
+        // event.preventDefault();
+        // if (section.children.length) {
+        //     this.toggleColapseMenu(section);
+        // }
+        // console.log(section.url, url);
+        // this.currentSection = section;
+        // this._sidebar.currentSection.next(section);
     }
-  }
 
-  activateCollapse(array: any, id: number) {
-    array.some((o: SideBarItemModel) => {
-      o.id === id
-        ? (o.collapsed = true)
-        : this.activateCollapse(o.children || [], id);
-    });
-  }
+    updateCollapseState (menu: Array<SideBarItemModel>) {
+        // if (this.currentSection && this.sectionSequence && this.sectionSequence.length === this.level && menu && menu.length > 0) {
+        // menu.forEach(item => {
+        //     item.collapsed = true;
+        //     // * El id del elemento es el de la sección seleccionada
+        //     if (item.id === this.currentSection.id) {
+        //         // console.log('ITEM SELECCIONADO', item.title);
+        //         item.collapsed = false;
+        //         item.selected = true;
 
-  updateCollapseState(menu: Array<SideBarItemModel>) {
-    if (this.currentSection && menu) {
-      // menu.forEach(item => {
-      //     item.collapsed = true;
-      //     // * El id del elemento es el de la sección seleccionada
-      //     if (item.id === this.currentSection.id) {
-      //         // console.log('ITEM SELECCIONADO', item.title);
-      //         item.collapsed = false;
-      //         item.selected = true;
+        //         // * El elemento no es el seleccionado y tiene hijos
+        //     } else if (item.children && item.children.length > 0) {
 
-      //         // * El elemento no es el seleccionado y tiene hijos
-      //     } else if (item.children && item.children.length > 0) {
+        //         // * Si uno de sus hijos es el elemento seleccionado
+        //         if (item.children.find(f => f.id === this.currentSection.id)) {
+        //             item.collapsed = false;
+        //         } else {
+        //             // console.log(`RECURSIVA de ${item.title}`, item.children);
+        //             this.updateCollapseState(item.children);
+        //         }
+        //     } else item.collapsed = true;
+        //     console.log(`${item.title} -> ${item.collapsed ? 'CERRADO' : 'ABIERTO'}`);
+        //     // debugger
+        // });
 
-      //         // * Si uno de sus hijos es el elemento seleccionado
-      //         if (item.children.find(f => f.id === this.currentSection.id)) {
-      //             item.collapsed = false;
-      //         } else {
-      //             // console.log(`RECURSIVA de ${item.title}`, item.children);
-      //             this.updateCollapseState(item.children);
-      //         }
-      //     } else item.collapsed = true;
-      //     console.log(`${item.title} -> ${item.collapsed ? 'CERRADO' : 'ABIERTO'}`);
-      //     // debugger
-      // });
+        if (this.currentSection) {
+            if (this.level === 1) console.log('>>>>>>>>>>>>>>>>>>>>>>', menu);
 
-      menu.forEach((item) => {
-        if (this.sectionSecuence[this.level - 1]) {
-          item.collapsed =
-            item.id === this.sectionSecuence[this.level - 1].id ? false : true;
-          if (!item.collapsed && item.children && item.children.length > 0) {
-            this.updateCollapseState(item.children);
-          } else item.collapsed = true;
-        } else item.collapsed = true;
-        console.log(
-          `${item.title} :: ${
-            this.sectionSecuence[this.level - 1].title
-          } ::::: ${item.collapsed}`
-        );
-      });
+            menu.forEach((item) => {
+                item.collapsed = this.currentSection.url.includes(item.url) ? false : true;
+                console.log(this.currentSection.url, item.url, this.currentSection.url.includes(item.url));
+                // item.collapsed = item.fatherSection.id === this.sectionSequence[this.level - 1].id ? false : true;
+
+                if (!item.collapsed && item.children && item.children.length > 0) {
+                    this.updateCollapseState(item.children);
+                } else item.collapsed = true;
+
+                // } else item.collapsed = true;
+                console.log(`ITEM : ${item.title}`);
+                // console.log(`SECTION SEQUENCE : ${this.sectionSequence[this.level - 1].title}`);
+                console.log(`ITEM COLLAPSED : ${item.collapsed}`);
+                console.log('========================================');
+            });
+        }
     }
-  }
 
-  assingParents() {
-    this.menu.forEach((rootItem) => {
-      if (this.parentSection) rootItem.fatherSection = this.parentSection;
-    });
-  }
+    public toggleColapseMenu (menuItem: SideBarItemModel): void {
+        if (menuItem.collapsed) this.collapseAll(this.menu);
+        menuItem.collapsed = !menuItem.collapsed;
+    }
 
-  ngOnDestroy() {
-    this.currentSectionSubscription.unsubscribe();
-  }
+    checkCollapsedSection (section: SideBarItemModel) {
+        if (section && section.fatherSection !== null) {
+            this.checkCollapsedSection(section.fatherSection);
+            this.activateCollapse(this.menu, section.id);
+        }
+    }
+
+    activateCollapse (array: any, id: number) {
+        array.some((o: SideBarItemModel) => {
+            o.id === id
+                ? (o.collapsed = true)
+                : this.activateCollapse(o.children || [], id);
+        });
+    }
+
+    assingParents () {
+        this.menu.forEach((rootItem) => {
+            if (this.parentSection) rootItem.fatherSection = this.parentSection;
+        });
+    }
+
+    ngOnDestroy () {
+        this.currentSectionSubscription.unsubscribe();
+    }
 }
