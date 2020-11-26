6fd824c8276bd7d9b2bb8278e8f16ee822f56c4d
Pedro <pedro.gragerafernandez@plexus.es>
2020-11-26 14:45:53 +0100

    Quitar console logs

--- src/app/core/components/modals/principal-treatment-modal/principal-treatment-modal.component.ts
+++ src/app/core/components/modals/principal-treatment-modal/principal-treatment-modal.component.ts
@@ -137,7 +137,7 @@ export class PrincipalTreatmentModalComponent implements OnInit {
 
   checkTypeTreatment(key): boolean {
     let show = true;
-    debugger
+    debugger;
     // Si estamos en cambiar o suspender
     // TODO: comparar si estamos en cambiar o suspender, si es así, deshabilitar/habilitar los campos correspondientes
     if (
