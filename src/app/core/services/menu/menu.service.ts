4f81c2b6e2c9e535215ea507803d109ea26cb51e
Lorena Sineiro García <lorena.sineirogarcia@plexus.es>
2020-12-17 16:50:28 +0100

    clean current patient when current section isnt in patient dashboard

--- src/app/core/services/menu/menu.service.ts
+++ src/app/core/services/menu/menu.service.ts
@@ -9,6 +9,7 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { FormsService } from '../forms/forms.service';
 import { TranslateService } from '@ngx-translate/core';
 import { ifError } from 'assert';
+import { PatientModel } from 'src/app/modules/pathology/patients/models/patient.model';
 
 @Injectable({
     providedIn: 'root',
@@ -17,10 +18,13 @@ export class MenuService {
     private current: MenuItemModel;
     private fullMenu: MenuItemModel;
     private homeUrl = '/hopes';
+    private patientPath: string;
 
     public currentSection = new Subject<MenuItemModel>();
     public allSections: MenuItemModel[];
     public thereIsPatientSelected: boolean = false;
+
+
 
     constructor(private _httpClient: HttpClient, private _formService: FormsService, private _modalService: NgbModal, private translate: TranslateService, private _router: Router) {
         this.fullMenu = JSON.parse(localStorage.getItem('completeMenu'));
@@ -28,9 +32,10 @@ export class MenuService {
         this.getSideBar();
     }
 
-    private assignParentAndCollapseStatus(menu: MenuItemModel, root?: string) {
+    private assignParentAndCollapseStatus (menu: MenuItemModel, root?: string) {
         menu.collapsed = true;
         menu.path = `${root ? root : ''}/${menu.id}`;
+        if (menu.url === '/hopes/pathology/patients') this.patientPath = menu.path;
         if (menu.children && menu.children.length > 0) {
             menu.children.forEach((submenu) => {
                 submenu.parentId = menu.id;
@@ -43,7 +48,7 @@ export class MenuService {
         return menu;
     }
 
-    private findSectionByUrl(menu: Array<MenuItemModel>, url: string): MenuItemModel {
+    private findSectionByUrl (menu: Array<MenuItemModel>, url: string): MenuItemModel {
         menu.forEach((item) => {
             if (url.indexOf('hopes') < 0) {
                 if (!url.startsWith('/')) url = '/hopes/' + url;
@@ -56,7 +61,7 @@ export class MenuService {
         return this.current;
     }
 
-    public fillSections(section: MenuItemModel) {
+    public fillSections (section: MenuItemModel) {
         if (!this.allSections) this.allSections = [];
         section.visible = true;
         this.allSections.push(section);
@@ -67,38 +72,42 @@ export class MenuService {
             });
     }
 
-    public setCurrentSection(section?: MenuItemModel) {
+    public setCurrentSection (section?: MenuItemModel) {
         if (!this._formService.getMustBeSaved() || (this._formService.getMustBeSaved() && this._formService.getSavedForm())) {
+            // * SE PROCEDE AL CAMBIO DE SECCIÓN * //
             if (!section) {
                 if (!this.allSections) this.fillSections(this.fullMenu);
                 section = this.allSections.filter((f) => f.url === '/hopes')[0];
             }
-
             const url = section.url === this.homeUrl ? this.homeUrl : section.url.split('/hopes')[1];
+
+            if (!section.path.includes(this.patientPath) && this.thereIsPatientSelected) { 
+                this.thereIsPatientSelected = false;
+                localStorage.removeItem('selectedPatient');
+                this.assignParentAndCollapseStatus(this.fullMenu);
+            }
 
             this.current = section;
             localStorage.setItem('section', JSON.stringify(section));
             if (url) this._router.navigate([url]);
             this.currentSection.next(section);
         } else {
-            if (section && section.url != '#' && section.url != this.current.url) {
-                this.showModalConfirm(section);
-            } else if (!section) {
-                this.showModalConfirm();
-            }
+            // * SE EVITA EL CAMBIO DE SECCIÓN * //
+            if (section && section.url != '#' && section.url != this.current.url) this.showModalConfirm(section);
+            else if (!section) this.showModalConfirm();
         }
     }
 
-    public setCurrentSectionByUrl(url: string) {
+    public setCurrentSectionByUrl (url: string) {
         const menu: MenuItemModel = JSON.parse(localStorage.getItem('completeMenu'));
         if (menu) this.setCurrentSection(this.findSectionByUrl(menu.children, url));
     }
 
-    public getCurrentSection(): Observable<MenuItemModel> {
+    public getCurrentSection (): Observable<MenuItemModel> {
         return this.currentSection.asObservable();
     }
 
-    public getSideBar(): Observable<MenuItemModel> {
+    public getSideBar (): Observable<MenuItemModel> {
         return this._httpClient.get<MenuItemModel>('/menus').pipe(
             map((response) => {
                 this.allSections = [];
@@ -110,7 +119,7 @@ export class MenuService {
             })
         );
     }
-    private showModalConfirm(section?: MenuItemModel) {
+    private showModalConfirm (section?: MenuItemModel) {
         const modalRef = this._modalService.open(ConfirmModalComponent);
         modalRef.componentInstance.title = this.translate.instant('saveWarning');
         modalRef.componentInstance.messageModal = this.translate.instant('saveWarningMessage');
