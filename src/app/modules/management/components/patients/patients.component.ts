60fff1d65b2eede987a60f389e7cc84a4c525a79
Lorena Sineiro García <lorena.sineirogarcia@plexus.es>
2021-02-01 12:41:45 +0100

    FIX patients component save

--- src/app/modules/management/components/patients/patients.component.ts
+++ src/app/modules/management/components/patients/patients.component.ts
@@ -51,9 +51,9 @@ export class PatientsComponent implements OnInit {
         private _notification: NotificationService,
         private _formBuilder: FormBuilder,
         private _hospitalService: HospitalService
-    ) {}
+    ) { }
 
-    ngOnInit(): void {
+    ngOnInit (): void {
         this.hospitals = this._activatedRoute.snapshot.data.hospitals;
         // this.patients = this._activatedRoute.snapshot.data.patients.content;
         //this.pathologies = this._activatedRoute.snapshot.data.hospitals[0].services[3].pathologies;
@@ -88,7 +88,7 @@ export class PatientsComponent implements OnInit {
         });
     }
 
-    public onSelectedItem(event: number): void {
+    public onSelectedItem (event: number): void {
         this.selectedPatient = this.patients[event];
         const selectedUser = JSON.stringify(this.selectedPatient || {});
         localStorage.setItem('selectedPatient', selectedUser);
@@ -100,25 +100,25 @@ export class PatientsComponent implements OnInit {
         });
     }
 
-    public getPatients() {
+    public getPatients () {
         this._patientsService.getPatients(this.selectedUser.rolSelected.pathology.id, '').subscribe((data) => {
             this.patients = data.content;
         });
     }
 
-    public getPathologiesIds() {
+    public getPathologiesIds () {
         this.pathologies.forEach((pathology) => {
             this.pathologiesIds.push(pathology.id);
         });
     }
 
-    public onSearch(event: string): void {
+    public onSearch (event: string): void {
         this._patientsService.findPatients(this.selectedUser.rolSelected.pathology.id, event).subscribe((data) => {
             this.patients = data.content;
         });
     }
 
-    public onIconButtonClick(event: any) {
+    public onIconButtonClick (event: any) {
         if (event && event.type === 'edit') {
             if (this.selectedPatient) {
                 this.modalForm.controls['pathology'].setValue(this.pathologies);
@@ -129,7 +129,7 @@ export class PatientsComponent implements OnInit {
         }
     }
 
-    public onSort(event: any) {
+    public onSort (event: any) {
         this.colOrder = event.column;
         this.typeOrder = event.direction;
         let query = `&sort=${this.colOrder},${this.typeOrder}&page=${this.currentPage}`;
@@ -141,12 +141,12 @@ export class PatientsComponent implements OnInit {
         this.refreshData(query);
     }
 
-    public selectItemsPerPage(number: number) {
+    public selectItemsPerPage (number: number) {
         this.itemsPerPage = number;
         this.selectPage(0);
     }
 
-    public savePatient(): void {
+    public savePatient (): void {
         this.isEditing = false;
         this.selectedItem = null;
         this.modalForm.reset();
@@ -154,12 +154,12 @@ export class PatientsComponent implements OnInit {
         this.showModal();
     }
 
-    public editPatient(): void {
+    public editPatient (): void {
         this.isEditing = true;
         this.showModal();
     }
 
-    public deletePatient(): void {
+    public deletePatient (): void {
         this._patientsService.deletePatient(this.patients[this.selectedItem].id).subscribe(
             (response) => {
                 this._notification.showSuccessToast('elementDeleted');
@@ -171,7 +171,7 @@ export class PatientsComponent implements OnInit {
         );
     }
 
-    private showModalConfirm() {
+    private showModalConfirm () {
         const modalRef = this._modalService.open(ConfirmModalComponent);
 
         modalRef.componentInstance.title = 'Eliminar Paciente';
@@ -185,7 +185,7 @@ export class PatientsComponent implements OnInit {
         });
     }
 
-    private showModal() {
+    private showModal () {
         const modalRef = this._modalService.open(EditorModalComponent, {
             size: 'lg',
         });
@@ -225,7 +225,7 @@ export class PatientsComponent implements OnInit {
         });
     }
 
-    private saveOrUpdate(event: any, modalRef: any): void {
+    private saveOrUpdate (event: any, modalRef: any): void {
         const formValues: any = event.value;
         const birthDate = new Date(formValues.birthDate).toISOString();
         let id: string;
@@ -245,9 +245,9 @@ export class PatientsComponent implements OnInit {
                     }
                 }
             }
-        } else {
-            pathologies_aux.push(formValues.pathology[0]);
-        }
+        } else if (Array.isArray(formValues.pathology)) pathologies_aux.push(formValues.pathology[0]);
+        else pathologies_aux.push(formValues.pathology);
+
         const hospital = formValues.hospital ? (formValues.hospital[0] ? formValues.hospital[0] : formValues.hospital) : formValues.hospital;
         const patient: PatientModel = new PatientModel(
             id,
@@ -290,7 +290,7 @@ export class PatientsComponent implements OnInit {
         }
     }
 
-    public selectPage(page: number): void {
+    public selectPage (page: number): void {
         let query: string;
         if (this.colOrder && this.typeOrder) {
             query = `&sort=${this.colOrder},${this.typeOrder}&page=${page}`;
@@ -304,7 +304,7 @@ export class PatientsComponent implements OnInit {
         this.refreshData(query);
     }
 
-    private refreshData(query: string): void {
+    private refreshData (query: string): void {
         this._patientsService.getPatients(this.selectedUser.rolSelected.pathology.id, query).subscribe((data) => {
             this.patients = data.content;
             this.paginationData.totalElements = data.totalElements;
