74467837b55bb7d6abe2d82fd4e18ebee5ed70ee
Emilio <emilio.castillogonzalez@plexus.es>
2021-02-15 17:29:13 +0100

    WIP: Principal treatment

--- src/app/modules/pathology/modules/dermatology/services/derma-treatments.service.ts
+++ src/app/modules/pathology/modules/dermatology/services/derma-treatments.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Pagination } from 'src/app/core/models/pagination/pagination/pagination.model';
 
 @Injectable({
     providedIn: 'root',
@@ -8,7 +9,26 @@ import { Observable } from 'rxjs';
 export class DermaTreatmentsService {
     constructor(private _http: HttpClient) {}
 
-    getAll(): Observable<any> {
-        return this._http.get(`/patients-treatments/find-all-treatments`);
+    private readonly endPoint = '/patients-treatments/find-all-treatments';
+
+    public getAll(patientId: string): Observable<Pagination<any>> {
+        const params: HttpParams = new HttpParams().set('patientId', patientId);
+        return this._http.get<any>(this.endPoint, { params });
+    }
+
+    public createTreatment(patientId: string, treatment: any): Observable<void> {
+        return this._http.put<void>(`${this.endPoint}/${patientId}`, treatment);
+    }
+
+    public updateTreatment(patientId: string, treatment: any): Observable<void> {
+        return this._http.post<void>(`${this.endPoint}/${patientId}`, treatment);
+    }
+
+    public suspendTreatment(patientId: string, treatment: any): Observable<void> {
+        return this._http.post<void>(`${this.endPoint}/${patientId}/suspend`, treatment);
+    }
+
+    public deleteTreatment(patientId: string, treatmentId: string): Observable<void> {
+        return this._http.delete<void>(`${this.endPoint}/${patientId}/${treatmentId}`);
     }
 }
