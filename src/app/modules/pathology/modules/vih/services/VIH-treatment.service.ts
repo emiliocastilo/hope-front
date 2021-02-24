6c4c9956fd1b20e57bef4675f2a30b98123b767b
Lorena Sineiro García <lorena.sineirogarcia@plexus.es>
2021-02-24 13:05:55 +0100

    FEAT interceptor accepts assets files, template reset const updated

--- src/app/modules/pathology/modules/vih/services/VIH-treatment.service.ts
+++ src/app/modules/pathology/modules/vih/services/VIH-treatment.service.ts
@@ -5,7 +5,6 @@ import { Injectable } from '@angular/core';
     providedIn: 'root',
 })
 export class VIHTreatmentService {
-    // private vihDataPath: string = '../../../../../../assets/data/vih';
     private vihDataPath: string = '/assets/data/vih';
     constructor(private _httpClient: HttpClient) {}
 
