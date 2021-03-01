4413f904d43d25ca23379fe47c04e2dc7ae26776
Lorena Sineiro García <lorena.sineirogarcia@plexus.es>
2021-03-01 12:19:12 +0100

    FIX translations, FIX creation of validation splitter instead of using general , mark to split validators

--- src/app/core/enum/template-cases.enum.ts
+++ src/app/core/enum/template-cases.enum.ts
@@ -1,3 +1,20 @@
-export const CleanOnInitTemplates: string[] = ['adherence-to-treatment-haynes', 'adherence-to-treatment-morisky', 'dlqi', 'leukocyte-antibody-antigen', 'napsi', 'pasi-bsa-pga', 'physical-condition'];
+export const CleanOnInitTemplates: string[] = [
+    // * DERMA * //
+    'adherence-to-treatment-haynes',
+    'adherence-to-treatment-morisky',
+    'dlqi',
+    'leukocyte-antibody-antigen',
+    'napsi',
+    'pasi-bsa-pga',
+    'physical-condition',
+
+    // * VIH *//
+    'adherence-to-treatment-SMAQ',
+];
+
+export const PartialCleanOnInitTemplates: string[] = [
+    // * VIH * //
+    'cardiovascular-risk'
+];
 
 export const AvoidSaveMessageTemplates: string[] = ['personal-information'];
